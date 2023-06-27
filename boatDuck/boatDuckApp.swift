67d146dde06486e8df80a5a6da9ebc403e597da5
Anjalee <anjaleeannar@gmail.com>
2023-06-27 10:52:12 -0400

    Initial Commit

---
+++ boatDuck/boatDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  boatDuckApp.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct boatDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
