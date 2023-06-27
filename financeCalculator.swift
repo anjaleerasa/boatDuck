5b823f2940d676456c8e41ac0caf7bb9f332747b
Anjalee <anjaleeannar@gmail.com>
2023-06-27 11:14:49 -0400

    hi

---
+++ financeCalculator.swift
@@ -0,0 +1,20 @@
+//
+//  financeCalculator.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct financeCalculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct financeCalculator_Previews: PreviewProvider {
+    static var previews: some View {
+        financeCalculator()
+    }
+}
