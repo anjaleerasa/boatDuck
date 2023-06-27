cbf45071a81a281a994fa261cb389e19cd02a628
Anjalee <anjaleeannar@gmail.com>
2023-06-27 11:20:47 -0400

    mattTest

---
+++ boatDuck/moneyFile.swift
@@ -0,0 +1,20 @@
+//
+//  moneyFile.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct moneyFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct moneyFile_Previews: PreviewProvider {
+    static var previews: some View {
+        moneyFile()
+    }
+}
