aedf886c2a6f52f211b99cafb1a7cf4dbeee7bfa
tanvi alok <talok535@gmail.com>
2023-06-28 14:01:56 -0400

    FIXED IT

---
+++ boatDuck/boatDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  boatDuckApp.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct boatDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
