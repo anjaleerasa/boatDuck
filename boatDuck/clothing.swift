aa61dce7073ceea0132b99efc93502cfbb5a1b9a
Tea Huang <Teaghuang@gmail.com>
2023-06-29 10:52:47 -0400

    ytgcsvdhbfjgnh

---
+++ boatDuck/clothing.swift
@@ -0,0 +1,20 @@
+//
+//  clothing.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct clothing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct clothing_Previews: PreviewProvider {
+    static var previews: some View {
+        clothing()
+    }
+}
