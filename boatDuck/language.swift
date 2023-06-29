78884a60da4e3366fa24aff1e2adbcda2aeba3c8
tanvi alok <talok535@gmail.com>
2023-06-29 14:40:44 -0400

    page

---
+++ boatDuck/language.swift
@@ -0,0 +1,20 @@
+//
+//  language.swift
+//  boatDuck
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+struct language: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct language_Previews: PreviewProvider {
+    static var previews: some View {
+        language()
+    }
+}
