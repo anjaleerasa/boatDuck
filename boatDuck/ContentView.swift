e898dcccd4c2191b172a443fd2f46055e08db725
Ima Paul <imap24680@gmail.com>
2023-06-30 09:56:53 -0400

    activities

--- boatDuck/ContentView.swift
+++ boatDuck/ContentView.swift
@@ -12,40 +12,40 @@ struct ContentView: View {
     @State private var cultureButt = false
     @State private var nestedCultureButt = false
     var body: some View {
-            ZStack{
-                Color("tessaColor")
-                    .edgesIgnoringSafeArea(.all)
-                VStack{
-                    Image("map")
+        ZStack{
+            Color("tessaColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("map")
+                    .resizable()
+                    .frame(width:350, height: 300)
+                Text("✿-where would you like to go?-✿")
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.107, brightness: 0.565))
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                
+                Text("click the icon that fits the location that you wish to visit")
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.112, brightness: 0.369))
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                Button{
+                    japanButt = true
+                }label: {
+                    Image("duckIcon")
                         .resizable()
-                        .frame(width:350, height: 300)
-                    Text("✿-where would you like to go?-✿")
-                        .foregroundColor(Color(hue: 1.0, saturation: 0.107, brightness: 0.565))
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                    
-                    Text("click the icon that fits the location that you wish to visit")
-                        .foregroundColor(Color(hue: 1.0, saturation: 0.112, brightness: 0.369))
-                        .font(.footnote)
-                        .multilineTextAlignment(.center)
-                    Button{
-                        japanButt = true
-                    }label: {
-                        Image("duckIcon")
-                            .resizable()
-                            .frame(width:100, height: 100)
-                    }
-                    Text("✿-CLICK SAILOR-✿")
-                        .font(.footnote)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(hue: 1.0, saturation: 0.061, brightness: 0.577))
-                        .multilineTextAlignment(.center)
-                    
-        
+                        .frame(width:100, height: 100)
                 }
+                Text("✿-CLICK SAILOR-✿")
+                    .font(.footnote)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.061, brightness: 0.577))
+                    .multilineTextAlignment(.center)
+                
+                
             }
-            .sheet(isPresented: $japanButt){
-                NavigationStack{
+        }
+        .sheet(isPresented: $japanButt){
+            NavigationStack{
                 ZStack{
                     Color("colorSheet")
                         .ignoresSafeArea(.all)
@@ -62,32 +62,30 @@ struct ContentView: View {
                                     .resizable()
                                 .frame (width:150, height:150) })
                         }
-                            HStack{
-                                NavigationLink(destination: language(), label: {
-                                    Image("leftBlue")
-                                        .resizable()
-                                    .frame (width:150, height:150) })
-                                Image("swimDuck")
+                        HStack{
+                            NavigationLink(destination: language(), label: {
+                                Image("leftBlue")
                                     .resizable()
-                                    .frame(width:100, height:100)
-                                NavigationLink(destination: clothing(), label: {
-                                    Image("rightBlue")
-                                        .resizable()
-                                    .frame (width:150, height:150) })
-                                
-                            }
-                            HStack{
-                                Spacer()
-                                NavigationLink(destination: financeCalculator(), label: {
-                                    Image("lowerLeft")
-                                        .resizable()
-                                    .frame (width:150, height:150) })
-                                Button{
-                                    nestedCultureButt = true
-                                }label:{
+                                .frame (width:150, height:150) })
+                            Image("swimDuck")
+                                .resizable()
+                                .frame(width:100, height:100)
+                            NavigationLink(destination: clothing(), label: {
+                                Image("rightBlue")
+                                    .resizable()
+                                .frame (width:150, height:150) })
+                            
+                        }
+                        HStack{
+                            Spacer()
+                            NavigationLink(destination: financeCalculator(), label: {
+                                Image("lowerLeft")
+                                    .resizable()
+                                .frame (width:150, height:150) })
+                                NavigationLink(destination: activities(), label: {
                                     Image("lowerRight")
                                         .resizable()
-                                    .frame(width:150, height:150)}
+                                    .frame (width:150, height:150) })
                                 Spacer()
                             }
                         }
@@ -98,10 +96,11 @@ struct ContentView: View {
             }//end of sheet
         }//end of body
     }
-                
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
